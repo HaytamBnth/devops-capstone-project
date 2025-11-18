ab38694921a9fb1e8d17caa4779e5470f10e9453
Haytam Bentouihar <152106324+HaytamBnth@users.noreply.github.com>
2025-11-18 03:45:48 +0100

    Clean up blank lines in service/routes.py
    Removed unnecessary blank lines in the routes.py file.

--- service/routes.py
+++ service/routes.py
@@ -57,6 +57,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
@@ -112,8 +113,6 @@ def update_accounts(account_id):
     return account.serialize(), status.HTTP_200_OK
 
 
-
-
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
@@ -134,8 +133,6 @@ def delete_accounts(account_id):
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
-
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
